a188ad2a475c072bb065867a97790f05b5d5e37f
Akash1Verma2 <ankitverma8621@gmail.com>
2026-04-05 09:19:41 +0530

    Creating New Route

--- app.js
+++ app.js
@@ -21,14 +21,35 @@ async function main(){
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hi, I am root!');
 });
 
+//Index Route - Show all listings
 app.get('/listings', async (req, res) => {
     const allListings = await Listing.find({});
-    res.render('listings/index', { allListings });
+    res.render('listings/index.ejs', { allListings });
+});
+
+//New Route - Show form to create a new listing
+app.get('/listings/new', (req, res) => {
+    res.render('listings/new.ejs');
+});
+
+//Show Route - Show details of a listing
+app.get('/listings/:id', async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render('listings/show.ejs', { listing });
+});
+
+//Create Route - Create a new listing
+app.post('/listings', async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect('/listings');
 });
 
 // app.get('/listings', async (req, res) => {
